a793ff6f002affce9af71919563c1e5815e167b4
jahirulislam <jahirulislam.dev@gmail.com>
2024-11-06 14:50:30 +0600

    making form using Formik package

--- travelapplication/components/screen/authentification/SignIn.jsx
+++ travelapplication/components/screen/authentification/SignIn.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
-
+import { Formik } from "formik";
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View style={styles.container}>
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <TextInput
+              style={{ borderColor: "black", borderWidth: 1 }}
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+            <Button onPress={handleSubmit} title="Submit" />
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };
 
 export default SignIn;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+});
